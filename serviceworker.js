ab26bc4cce92bb68cc807a207accf09347ccf4e9
Cristian Ismael Gramajo <cristiangramajo015@gmail.com>
2023-05-04 11:44:12 -0300

    Agregado HTML en service worker

--- serviceworker.js
+++ serviceworker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "portfolio-v23";
-const urlsToCache = ["./","./css/BebasNeue-Regular.woff", "./css/BebasNeue-Regular.woff2", "https://upload.wikimedia.org/wikipedia/commons/c/c5/Colorwheel.svg"];
+const urlsToCache = ["./","./index.html","./css/BebasNeue-Regular.woff", "./css/BebasNeue-Regular.woff2", "https://upload.wikimedia.org/wikipedia/commons/c/c5/Colorwheel.svg"];
 
 // si cambiamos la version de cache se elimina la anterior
 caches.keys().then(res=>{
